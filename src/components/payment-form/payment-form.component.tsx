3fecd50be81f86db8faa77f8cd3ee1eeb1478366
idilkel <96882056+idilkel@users.noreply.github.com>
2022-11-02 17:15:25 +0200

    small commenting

--- src/components/payment-form/payment-form.component.tsx
+++ src/components/payment-form/payment-form.component.tsx
@@ -3,11 +3,11 @@ import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import { useSelector, useDispatch } from "react-redux";
 
 import { selectCartTotal } from "../../store/cart/cart.selector";
-import { selectCurrentUser } from "../../store/user/user.selector";
+//import { selectCurrentUser } from "../../store/user/user.selector";
 import { selectCartItems } from "../../store/cart/cart.selector";
 
 import { BUTTON_TYPE_CLASSES } from "../button/button.component";
-import FormInput from "../form-input/form-input.component";
+//import FormInput from "../form-input/form-input.component";
 
 import { clearCart } from "../../store/cart/cart.action";
 
@@ -24,7 +24,7 @@ const PaymentForm = () => {
   const dispatch = useDispatch();
   const elements = useElements();
   const amount = useSelector(selectCartTotal);
-  const currentUser = useSelector(selectCurrentUser);
+  //const currentUser = useSelector(selectCurrentUser);
   const cartItems = useSelector(selectCartItems);
   const [isProcessingPayment, setIsProcessingPayment] = useState(false);
   const [cardHolderName, setCardHolderName] = useState("");
@@ -43,19 +43,23 @@ const PaymentForm = () => {
     const response = await fetch("/.netlify/functions/create-payment-intent", {
       method: "post",
       headers: {
-        "content-type": "application/json",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ amount: amount * 100 }),
     }).then((res) => res.json());
 
-    //console.log(response);
+    console.log("response!!!: " + response);
 
     //const clientSecret = response.paymentIntent.client_secret;
     const {
       paymentIntent: { client_secret },
     } = response;
 
+    console.log("response!!!: " + response);
+
     const cardDetails = elements.getElement(CardElement);
+
+    console.log("card details!!!: " + cardDetails);
 
     if (cardDetails === null) return;
 
