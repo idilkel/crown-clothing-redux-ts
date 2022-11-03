597ad1e9dd59132d08c8f80c5810f57b165b2487
idilkel <96882056+idilkel@users.noreply.github.com>
2022-11-03 09:41:19 +0200

    bundle.js dismantled to lazy imports on App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useEffect } from "react";
+//lazy,Suspense: to dismantle the initial bundle.js to parts and get them lazy as async
+import { useEffect, lazy, Suspense } from "react";
 import { useDispatch } from "react-redux";
 
 import { Routes, Route } from "react-router-dom";
@@ -9,12 +10,23 @@ import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
 } from "./utils/firebase/firebase.utils";
+import Spinner from "./components/spinner/spinner.components";
 
-import Home from "./routes/home/home.components";
-import Navigation from "./routes/navigation/navigation.component";
-import Authentication from "./routes/authentication/authentication.component";
-import Shop from "./routes/shop/shop.component";
-import Checkout from "./routes/checkout/checkout.component";
+//import Home from "./routes/home/home.components";
+// import Navigation from "./routes/navigation/navigation.component";
+// import Authentication from "./routes/authentication/authentication.component";
+// import Shop from "./routes/shop/shop.component";
+// import Checkout from "./routes/checkout/checkout.component";
+
+const Home = lazy(() => import("./routes/home/home.components"));
+const Navigation = lazy(() =>
+  import("./routes/navigation/navigation.component")
+);
+const Authentication = lazy(() =>
+  import("./routes/authentication/authentication.component")
+);
+const Shop = lazy(() => import("./routes/shop/shop.component"));
+const Checkout = lazy(() => import("./routes/checkout/checkout.component"));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -35,14 +47,16 @@ const App = () => {
   }, []);
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+    <Suspense fallback="<Spinner/>">
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
